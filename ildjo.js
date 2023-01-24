25eda3ca36eb2675f52c54b516a95665873e0ceb
firussgit <firussmail@gmail.com>
2023-01-24 16:45:58 +0100

    removed_loading

--- ildjo.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("load", () => {
-    document.querySelector(".loader").classList.add("loader--hidden")
-    document.querySelector(".loader").addEventListener("transitionend", () => {
-      document.body.removeChild(document.querySelector(".loader"));
-    });
-});
