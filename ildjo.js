bb4625e7369df3cc20bc07ee7e5084c1f367178e
firussgit <firussmail@gmail.com>
2023-02-06 13:57:23 +0100

    animated

---
+++ ildjo.js
@@ -0,0 +1,17 @@
+const animation_elements = document.querySelectorAll('.animate-on-scroll');
+
+const observer = new IntersectionObserver((entries) => {
+	entries.forEach((entry) => {
+		if (entry.isIntersecting) {
+			entry.target.classList.add('animate');
+		}
+	})
+}, {
+	threshold: 0.5
+});
+
+for (let i = 0; i < animation_elements.length; i++) {
+	const el = animation_elements[i];
+
+	observer.observe(el);
+}
