58bca975fd8b996e657392ad12453e156173db59
firussgit <firussmail@gmail.com>
2023-03-04 22:35:48 +0100

    update

--- website/js/scripts.js
+++ website/js/scripts.js
@@ -1,12 +1,7 @@
 const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+const navMenu = document.querySelector(".nav");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 })
-
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-}))
