2fb4928dc07f85512986e86a00fbec4db784ecbe
firussgit <firussmail@gmail.com>
2023-03-06 22:26:10 +0100

    reset

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-/* Navbar */
-
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav");
-
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
