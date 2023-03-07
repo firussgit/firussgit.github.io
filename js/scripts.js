90044fa1b555e1f2565c73839c095e27dd840583
firussgit <firussmail@gmail.com>
2023-03-07 19:15:17 +0100

    online

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+/* Navbar */
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
