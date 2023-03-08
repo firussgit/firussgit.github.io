39a9b08de8ba7c7fe93270899096b5766c6421c1
firussgit <firussmail@gmail.com>
2023-03-08 13:03:35 +0100

    theme switch added

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,18 @@ hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 })
+
+let mode = "light"
+
+function theme() {
+    if (mode == "light") {
+        document.getElementById("smh").classList.add("dark")
+        mode = "dark"
+        console.log(mode)
+    }
+    else {
+        document.getElementById("smh").classList.remove("dark")
+        mode = "light"
+        console.log(mode)
+    }
+}
